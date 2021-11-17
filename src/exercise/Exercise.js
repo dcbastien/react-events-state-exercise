91e8ae5d8c49e0a3d03ca098bc78e0e52a92f84f
dcbastien <ddcbastien@gmail.com>
2021-11-17 07:54:09 -0600

    Completed exercise w/o bonus

---
+++ src/exercise/Exercise.js
@@ -0,0 +1,31 @@
+//2a
+import { useState } from 'react';
+
+
+
+// 1a
+function Exercise (){
+
+    // 2c
+ const [num, setNum] = useState(1);
+ const 
+
+//  2d
+function addNum = setNum(num + 1)
+
+ //1c
+    return (
+    <div>
+        {/* 1e */}
+        <button onClick={()=> alert(`Congrats! You have clicked the button.`)}>CLICK HERE</button>
+        {/* 2b */}
+        <button>STATE</button>
+
+
+    </div>
+)
+
+
+}
+//1b
+export default Exercise;
